19a7e524650d1f1b1a27f93b4ce20ef1a583e2bd
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-10-02 11:56:12 +0200

    Commit 1

---
+++ app/src/main/java/com/example/photocryptography/PhotoCryptography.java
@@ -0,0 +1,24 @@
+package com.example.photocryptography;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.core.view.WindowCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.photocryptography.databinding.ActivityPhotoCryptographyBinding;
+
+public class PhotoCryptography extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
