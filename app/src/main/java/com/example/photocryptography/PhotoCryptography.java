63a769f3ceebb318f418121e9a37d7dab92a4e0c
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-10-05 10:26:30 +0200

    Commit 4

--- app/src/main/java/com/example/photocryptography/PhotoCryptography.java
+++ app/src/main/java/com/example/photocryptography/PhotoCryptography.java
@@ -1,34 +1,32 @@
 package com.example.photocryptography;
 
 import android.Manifest;
+import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.core.view.WindowCompat;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
-import com.example.photocryptography.databinding.ActivityPhotoCryptographyBinding;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 public class PhotoCryptography extends AppCompatActivity {
     private Button encrypt, decrypt;
@@ -61,8 +59,7 @@ public class PhotoCryptography extends AppCompatActivity {
                 if (ContextCompat.checkSelfPermission(PhotoCryptography.this,
                         Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
                     ActivityCompat.requestPermissions(PhotoCryptography.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 100);
-                }
-                else {
+                } else {
                     selectPhoto();
                 }
             }
@@ -87,5 +84,54 @@ public class PhotoCryptography extends AppCompatActivity {
         intent.setType("image/*");
         startActivityForResult(Intent.createChooser(intent, "Select picture"), 100);
 
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 100 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            selectPhoto();
+        } else {
+            Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100 && resultCode == RESULT_OK && data != null) {
+            Uri uri = data.getData();
+            Bitmap bitmap;
+            ImageDecoder.Source source = null;
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                source = ImageDecoder.createSource(getContentResolver(), uri);
+
+                try {
+                    bitmap = ImageDecoder.decodeBitmap(source);
+                    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+                    byte[] bytes = stream.toByteArray();
+                    image = Base64.encodeToString(bytes, Base64.DEFAULT);
+                    encImg.setText(image);
+                    Toast.makeText(this, "Image encrypted! Click on Decrypt to restore", Toast.LENGTH_SHORT).show();
+                } catch (IOException e) {
+                    Toast.makeText(this, "The image can not be encrypted.", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+
+        }
+
+    }
+
+    public void copyCode(View view) {
+        String codes = encImg.getText().toString().trim();
+        if(!codes.isEmpty()){
+            ClipData temp = ClipData.newPlainText("text", codes);
+            clipboardManager.setPrimaryClip(temp);
+            Toast.makeText(this, "Text copied to clipboard", Toast.LENGTH_SHORT).show();
+        }
     }
 }
